fb8032f052b6e031f503fff01ac78ce90b3dff0d
thodsapols <50854499+thodsapols@users.noreply.github.com>
2020-09-10 14:18:59 +0700

    update code
    update code

---
+++ GatewayPortal.JLK/Model/AnonymizeDataModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GatewayPortal.JLK.Model
+{
+    class AnonymizeDataModel
+    {
+        public string PatientName { get; set; }
+        public string PatientID { get; set; }
+        public string PatientBirthDate { get; set; }
+        public string PatientSex { get; set; }
+        public string PatientAge { get; set; }
+        public string PatientWeight { get; set; }
+        public string PatientAddress { get; set; }
+        public DateTime StudyDate { get; set; }
+        public DateTime StudyTime { get; set; }
+        public string StudyID { get; set; }
+        public string StudyModality { get; set; }
+        public string StudyDescription { get; set; }
+        public DateTime SeriesDate { get; set; }
+        public DateTime SeriesTime { get; set; }
+        public string SeriesDescription { get; set; }
+        public string AccessionNumber { get; set; }
+        public string InstitutionName { get; set; }
+
+    }
+}
